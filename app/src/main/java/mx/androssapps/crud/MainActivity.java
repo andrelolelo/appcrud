e2944bfb3b41b80a05ce9db72a10c258b5b8d85e
Andrés Eduardo Fuentes Cortázar <androssfu@gmail.com>
2019-06-10 00:41:52 -0500

    Terminado

--- app/src/main/java/mx/androssapps/crud/MainActivity.java
+++ app/src/main/java/mx/androssapps/crud/MainActivity.java
@@ -10,8 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -26,12 +28,16 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.HashMap;
 import java.util.Map;
 
+import mx.androssapps.crud.modelo.Persona;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText edtNombre, edtApellido, edtTelefono;
     Button btnAgregar, btnEliminar, btnModificar;
+    String nombre = "";
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference personaRef = db.collection("agenda");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         btnAgregar = (Button) this.findViewById(R.id.btnAgregar);
         btnEliminar = (Button) this.findViewById(R.id.btnEliminar);
         btnModificar = (Button) this.findViewById(R.id.btnModificar);
+
 
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,14 +91,81 @@ public class MainActivity extends AppCompatActivity {
         btnEliminar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                borrarPersona();
             }
         });
+
         btnModificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+
+
+                buscarPersona();
+                final String apellidoPersona = edtApellido.getText().toString();
+                final String telefonoPersona = edtTelefono.getText().toString();
+
+                Map<String, Object> editarPersona = new HashMap<>();
+                editarPersona.put("apellido", apellidoPersona);
+                editarPersona.put("telefono", telefonoPersona);
+
+                db.collection("agenda").document(nombre).update(editarPersona)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this, "¡Ha actualizado",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "No se pudo realizar la operación.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        
+            }
+
+
+        });
+    }
+
+    public void buscarPersona(){
+        Query query = personaRef.whereEqualTo("nombre", edtNombre.getText());
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document: task.getResult()){
+                        Persona usuario = document.toObject(Persona.class);
+                        edtApellido.setText(usuario.getApellido());
+                        edtTelefono.setText(usuario.getTelefono());
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "Error al recuperar el perfil", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
+
+    public void borrarPersona(){
+        CollectionReference resenasRef = db.collection("agenda");
+        Query queryEliminar = resenasRef.whereEqualTo("nombre", edtNombre.getText());
+
+        queryEliminar.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document: task.getResult()){
+                        String id = document.getId();
+                        db.collection("agenda").document(id).delete();
+                    }
+                }
+            }
+        });
+    }
+
+
+
 }
